550d312f5d80db9691d3b885412e0fd852b6de6e
Rainbow Shaw <85267879+Funny-Rainbow@users.noreply.github.com>
2022-12-25 05:09:39 +0800

    Add files via upload
    新增功能：
    打开大写锁进入连射功能（测试中）
    连射比单点表现更好
    
    新增多线程

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-import pyaudio
-import wave
-import numpy as np
-import pyautogui as mouse
+import pyaudio# 调用麦克风
+import wave# 音频格式
+import numpy as np# 音频处理
+import pyautogui as mouse# 鼠标控制
+import win32api# 检测大写锁
+import win32con# 检测大写锁
+import random# 随机，防止脚本检测
+import threading# 线程
 from time import sleep
 
-import random
-def Monitor():
-    CHUNK = 128
+
+# 0为大写关闭，连点；1为大写开启，按住鼠标左键
+Tab = [0]
+# 1开火
+shoot = [0]
+
+# 获取麦克风音量 函数
+def monitor():
+    # 设置录音片段的大小格式等
+    CHUNK = 512
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 48000
@@ -19,22 +30,27 @@ def Monitor():
     print("开始缓存录音")
     print('begin ')
     frames = []
-    while (True):
-        stream.start_stream()
-        #for i in range(0, 100):
+    # 进入音量识别循环
+    stream.start_stream()
+    while True:
+        global temp
+        # for i in range(0, 100):
         data = stream.read(CHUNK)
         frames.append(data)
         audio_data = np.fromstring(data, dtype=np.short)
-        large_sample_count = np.sum( audio_data > 800 )
+        large_sample_count = np.sum(audio_data > 800)
         temp = np.max(audio_data)
-        print(temp)
-        if temp > 1000: # | temp <2000
-            stream.stop_stream()
-            print("检测到信号")
-            print('当前阈值：',temp)
-            mouse.click(clicks=20, interval=random.uniform(0.01,0.05),button='left')
+        # print(temp)
+        # 如果最近声音大于设定值，这里默认为1000，点击鼠标
+        if temp < 2000:
+            shoot[0] = 0
+        else:
+            # 设置鼠标点击次数和时间，为防止识别为脚本，每次点击间隔使用0.01-0.05的随机数
+            Tab[0]=win32api.GetKeyState(win32con.VK_CAPITAL)
+            shoot[0] = 1
+        sleep(0.01)
+    # 以下代码非必要无需修改
     stream.stop_stream()
-    input("Press Enter key to exit.")
     stream.close()
     p.terminate()
     wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
@@ -44,5 +60,37 @@ def Monitor():
     wf.writeframes(b''.join(frames))
     wf.close()
 
+# 点击鼠标 函数
+def mouseLeftClick(Tab, shoot):
+    Tab_temp = Tab[0]
+    shoot_temp = shoot[0]
+    while True:
+        if shoot_temp == 1:
+            print('shooting!')
+            if Tab_temp == 1:
+                mouse.mouseDown(button='left')
+                sleep(0.5)
+                mouse.mouseUp(button='left')
+            elif Tab_temp == 0:
+                mouse.click(clicks=50, interval=random.uniform(0.01, 0.02), button='left')
+                sleep(0.05)
+            else:
+                print('error occurs:Tab status wrong')
+        print('stop')
+        # 只有在状态改变时才赋值
+        if Tab_temp != Tab[0] or shoot_temp != shoot[0]:
+            Tab_temp = Tab[0]
+            shoot_temp = shoot[0]
+
+
+
 if __name__ == '__main__':
-    Monitor()
+    # 双线程
+    t1 = threading.Thread(target=mouseLeftClick, args=(Tab,shoot,))
+    t2 = threading.Thread(target=monitor)
+    # 录音加上线程守护
+    t2.daemon=True
+    t1.start()
+    t2.start()
+
+
